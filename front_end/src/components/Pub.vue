c58ecd889cd578eaf233693ade93ad1eecb2825d
Guangzong Chen <chen@ggeta.com>
2025-01-29 11:24:58 -0500

    add publication and codeforces

---
+++ front_end/src/components/Pub.vue
@@ -0,0 +1,87 @@
+<template>
+      <h1>Publications</h1>
+
+    <div class="publication">
+        <a href="https://arxiv.org/abs/2411.10368" target="_blank">
+            <div class="title">Mechanisms of Generative Image-to-Image Translation Networks</div>
+        </a>
+        <div class="authors">Guangzong Chen, Mingui Sun, Zhi-Hong Mao, Kangni Liu, Wenyan Jia</div>
+        <div class="journal">arXiv preprint arXiv:2411.10368, 2024</div>
+    </div>
+
+    <div class="publication">
+        <a href="hhttps://openaccess.thecvf.com/content/CVPR2024W/MTF/html/Chen_Shape-Preserving_Generation_of_Food_Images_for_Automatic_Dietary_Assessment_CVPRW_2024_paper.html" target="_blank">
+            <div class="title">Shape-Preserving Generation of Food Images for Automatic Dietary Assessment</div>
+        </a>
+        <div class="authors">Guangzong Chen, Zhi-Hong Mao, Mingui Sun, Kangni Liu, Wenyan Jia</div>
+        <div class="journal">Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition, Pages 3721-3731, 2024</div>
+    </div>
+    
+    
+    <div class="publication">
+        <a href="https://www.frontiersin.org/journals/artificial-intelligence/articles/10.3389/frai.2021.644712/full" target="_blank">
+            <div class="title">Food/non-Food classification of real-life egocentric images in low-and middle-income countries based on image tagging features</div>
+        </a>
+        <div class="authors">Guangzong Chen, Wenyan Jia, Yifan Zhao, Zhi-Hong Mao, Benny Lo, Alex K Anderson, Gary Frost, Modou L Jobarteh, Megan A McCrory, Edward Sazonov, et al.</div>
+        <div class="journal">Frontiers in Artificial Intelligence, Volume 4, Pages 644712, 2021. Publisher: Frontiers Media SA</div>
+    </div>
+
+    <div class="publication">
+        <a href="https://www.mdpi.com/1424-8220/22/20/8006" target="_blank">
+            <div class="title">Improved wearable devices for dietary assessment using a new camera system</div>
+        </a>
+        <div class="authors">Mingui Sun, Wenyan Jia, Guangzong Chen, Mingke Hou, Jiacheng Chen, Zhi-Hong Mao</div>
+        <div class="journal">Sensors, Volume 22, Issue 20, Pages 8006, 2022. Publisher: MDPI</div>
+    </div>
+    
+    
+    <div class="publication">
+        <a href="https://www.sciencedirect.com/science/article/abs/pii/S0260877424000098" target="_blank">
+            <div class="title">Image-based volume estimation for food in a bowl</div>
+        </a>
+        <div class="authors">Wenyan Jia, Boyang Li, Qi Xu, Guangzong Chen, Zhi-Hong Mao, Megan A McCrory, Tom Baranowski, Lora E Burke, Benny Lo, Alex K Anderson, et al.</div>
+        <div class="journal">Journal of Food Engineering, Volume 372, Pages 111943, 2024. Publisher: Elsevier</div>
+    </div>
+    
+
+
+
+
+
+</template>
+
+
+<script>
+export default {
+    name: 'Pub'
+}
+</script>
+
+<style scoped>
+       body {
+            font-family: Arial, sans-serif;
+            margin: 40px;
+            line-height: 1.6;
+        }
+        .publication {
+            margin-bottom: 20px;
+            padding: 15px;
+            border: 1px solid #ddd;
+            border-radius: 8px;
+            background-color: #f9f9f9;
+        }
+        .title {
+            font-weight: bold;
+            font-size: 1.2em;
+        }
+        .authors {
+            font-style: italic;
+        }
+        .journal {
+            color: #555;
+        }
+        a {
+            text-decoration: none;
+            color: #007bff;
+        }
+</style>
